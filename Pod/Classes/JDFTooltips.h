c26a4f34c90a589d9162a26721d1e3ffd062b4f4
Joe Fryer <joe.d.fryer@gmail.com>
2015-07-29 11:18:00 +0100

    Tidying.

--- Pod/Classes/JDFTooltips.h
+++ Pod/Classes/JDFTooltips.h
@@ -15,33 +15,3 @@
 #import "JDFTooltipView.h"
 #import "JDFSequentialTooltipManager.h"
 #import "JDFTooltipManager.h"
-
-
-/**
- Todo (high priority):
- - Empty. Woo!
- */
-
-/**
- Todo (low priority):
- - Check the managers - will the view that is initially supplied as the hostView for a tooltip be superceded by the hostView on the manager? Is this correct?
- - If, for example, being shown from a non-full-screen modal on iPad, make sure it can overflow outside of the modal itself.
- - Maybe revisit the logic for positioning the tooltips (better centring etc)
- - In some cases (different positions), the tooltips 'move' if they are shown repeatedly.
- - Allow showing without an arrow
- - Allow showing an image/icon instead/as well?
- - Auto-dismiss option after specified time.
- - Managers - change the convenience methods to affect tooltips that are added later
- - Add the backdrop to the host instead of the host's window? Or the keyWindow?
- - Add completion blocks to the managers.
- - Documentation - mention that all tooltips in a single manager need to have the same hostView for orientation to work. (They do, don't they?).
- - Allow using tabBarItems as targets?
- - Add some kind of layout block, so that you can supply your own code for laying out the tooltip (useful for allowing you to use targetPoint and still suporting rotation).
- - More shadow customisation options?
- - setTooltipNeedsLayoutWithHostViewSize: - do we need to supply the size here?
- - Add the ability to adjust the positioning relative to a view (move further up by x etc)
- - JDFTooltipSequentialManager (sequentially show all in one go, with kind of quick bubbling effect as well?)
- - Managers - Completion blocks (on hide etc.)
-
- */
-
