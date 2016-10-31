7d62cefe5f1c07bcdb05b6f898d5e957eca5a269
Anders Friis <af@andersfr.dk>
2016-10-31 10:06:47 +0100

    Fix framework

--- Example/JDFTooltips/JDFTooltips.h
+++ Example/JDFTooltips/JDFTooltips.h
@@ -6,24 +6,8 @@
 //  Copyright © 2016 Joe Fryer. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
-//! Project version number for JDFTooltips.
-FOUNDATION_EXPORT double JDFTooltipsVersionNumber;
-
-//! Project version string for JDFTooltips.
-FOUNDATION_EXPORT const unsigned char JDFTooltipsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <JDFTooltips/PublicHeader.h>
-
-
-#ifndef JoeTooltips_JDFTooltips_h
-#define JoeTooltips_JDFTooltips_h
-
-
-#endif
-
-#import "JDFTooltipView.h"
-#import "JDFSequentialTooltipManager.h"
-#import "JDFTooltipManager.h"
+#import "JDFTooltips/JDFTooltipView.h"
+#import "JDFTooltips/JDFSequentialTooltipManager.h"
+#import "JDFTooltips/JDFTooltipManager.h"
 
