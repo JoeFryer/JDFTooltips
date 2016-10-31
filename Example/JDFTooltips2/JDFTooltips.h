871c231b63a2c4eb38c6e8a650e6fa17bdaa88cd
Anders Friis <af@andersfr.dk>
2016-10-31 09:44:13 +0100

    Fix framework

---
+++ Example/JDFTooltips2/JDFTooltips.h
@@ -0,0 +1,20 @@
+//
+//  JDFTooltips2.h
+//  JDFTooltips2
+//
+//  Created by Anders Friis on 31/10/2016.
+//  Copyright © 2016 Joe Fryer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDFTooltips2.
+FOUNDATION_EXPORT double JDFTooltips2VersionNumber;
+
+//! Project version string for JDFTooltips2.
+FOUNDATION_EXPORT const unsigned char JDFTooltips2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDFTooltips2/PublicHeader.h>
+
+
+#import <JDFTooltips/JDFTooltips.h>
